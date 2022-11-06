41cf4f15a2ea1fdaa75e8062c06d261a235d6a77
davydovMikhail <davydov27977@gmail.com>
2022-11-06 11:48:37 +0700

    contract and test

---
+++ contracts/StakeToken.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract StakeToken is ERC20 {
+
+    uint256 constant aHour = 1 hours;
+    uint256 constant aDay = 24 hours;
+    uint constant precision = 1000000000;
+
+    struct User {
+        uint stakedAmount;
+        uint stakeTime;
+        uint claimTime;
+    }
+
+    mapping(address => User) public users;
+
+    constructor(
+        uint256 _initialSupply
+    ) ERC20("MyTestCoint", "MTC") {
+        _mint(msg.sender, _initialSupply);
+    }
+
+    function stake(uint256 _amount) external {
+        address sender = _msgSender();
+        uint currentTime = block.timestamp;
+        require(_amount > 0, "Amount must be greater then zero.");
+        require(users[sender].stakedAmount == 0, "Your tokens is staked arleady.");
+        _transfer(sender, address(this), _amount);
+        users[sender] = User({
+            stakedAmount: _amount,
+            stakeTime: currentTime,
+            claimTime: currentTime
+        });
+    }
+
+    function claim() external {
+        address sender = _msgSender();
+         uint currentTime = block.timestamp;
+        User storage info = users[sender];
+        require(info.stakedAmount > 0, "Your tokens is not staked yet.");
+        uint distantSeconds = currentTime - info.claimTime;
+        require(distantSeconds >= aHour, "It's been less than an hour.");
+        uint share = _calcRewards(info.stakedAmount, distantSeconds);
+        _mint(sender, share); 
+        info.claimTime = currentTime;
+    }
+
+    function _calcRewards(uint _stakedAmount, uint _seconds) internal pure returns(uint share) {
+        uint hoursQuantity = ((_seconds * precision) / aHour); // 1000000000 * hoursQuantity
+        uint sharePerHour = (_stakedAmount * 100) / 1000;
+        share = (hoursQuantity * sharePerHour) / precision;
+    }
+
+    function unstake() external { 
+        address sender = _msgSender();
+        User storage info = users[sender];
+        require(info.stakedAmount > 0, "Your tokens is not staked yet.");
+        require(block.timestamp - info.stakeTime >= aDay, "The time for unstake has not yet come.");
+        _transfer(address(this), sender, info.stakedAmount);
+        info.stakedAmount = 0;
+    }
+}
---
+++ test/stake-token.ts
@@ -0,0 +1,126 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { parseEther } from '@ethersproject/units';
+
+describe("Stake token test", async function () {
+
+  async function deployStakeTokenAndPrepare() {
+
+    const totalSupply = parseEther('1000000');
+    const defaultUserBalance = parseEther('10000')
+    const [owner, user1, user2, user3] = await ethers.getSigners();
+
+    const StakeToken = await ethers.getContractFactory("StakeToken");
+    const stakeToken = await StakeToken.connect(owner).deploy(totalSupply);
+
+    await stakeToken.connect(owner).transfer(user1.address, defaultUserBalance);
+    await stakeToken.connect(owner).transfer(user2.address, defaultUserBalance);
+    await stakeToken.connect(owner).transfer(user3.address, defaultUserBalance);
+
+    return { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance };
+  }
+
+  async function timeMovement(_seconds: number) {
+    await ethers.provider.send("evm_increaseTime", [_seconds]);
+    await ethers.provider.send("evm_mine", []);
+  }
+
+  describe("Deployment", function () {
+    it("Should set the correct totalSupply", async function () {
+      const { stakeToken, totalSupply } = await loadFixture(deployStakeTokenAndPrepare);
+      expect(await stakeToken.totalSupply()).to.equal(totalSupply);
+    });
+
+    it("Should set the right balances by users", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      expect(await stakeToken.balanceOf(owner.address)).to.equal(totalSupply.sub(defaultUserBalance.mul(3)));
+      expect(await stakeToken.balanceOf(user1.address)).to.equal(defaultUserBalance);
+      expect(await stakeToken.balanceOf(user2.address)).to.equal(defaultUserBalance);
+      expect(await stakeToken.balanceOf(user3.address)).to.equal(defaultUserBalance);
+    });
+  });
+
+  describe("Staking workflow", function () {
+    it("User 1 stakes his coins and balances is changed", async function () {
+      const { stakeToken, user1, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      expect(await stakeToken.balanceOf(stakeToken.address)).to.equal(defaultUserBalance);
+      expect(await stakeToken.balanceOf(user1.address)).to.equal(0);
+    });
+
+    it("User 1 stakes his coins and then he claimed rewards in an hour and a half", async function () {
+      const { stakeToken, user1, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      const hourAndHalf = 60 * 60 * 1.5;
+      const estimatedRewards = parseEther(((10_000 / 10) * 1.5).toString());
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      await timeMovement(hourAndHalf);
+      await stakeToken.connect(user1).claim();
+      expect(await stakeToken.balanceOf(user1.address)).to.be.closeTo(estimatedRewards, parseEther('1'));
+    });
+
+    it("Check changing totalSupply after claim", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      const hoursQuantity = 60 * 60 * 10.3; // 10.3 hours
+      const estimatedRewards = parseEther(((10_000 / 10) * 10.3).toString());
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      await timeMovement(hoursQuantity);
+      await stakeToken.connect(user1).claim();
+      expect(await stakeToken.totalSupply()).to.be.closeTo(totalSupply.add(estimatedRewards), parseEther('1'));
+    });
+
+    it("Check correct work unstake",async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      const aDay = 60 * 60 * 24;
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      expect(await stakeToken.balanceOf(user1.address)).to.equal(0);
+      await timeMovement(aDay);
+      await stakeToken.connect(user1).unstake();
+      expect(await stakeToken.balanceOf(user1.address)).to.equal(defaultUserBalance);
+    });
+    
+  });
+
+  describe("Check staking requires", function () {
+    it("Repeated stake", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await stakeToken.connect(user1).stake(defaultUserBalance.div(2));
+      await expect(stakeToken.connect(user1).stake(defaultUserBalance.div(2))).to.be.revertedWith("Your tokens is staked arleady.");
+    });
+
+    it("Zero stake", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await expect(stakeToken.connect(user1).stake(0)).to.be.revertedWith("Amount must be greater then zero.");
+    });
+
+    it("Claim without stake", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await expect(stakeToken.connect(user1).claim()).to.be.revertedWith("Your tokens is not staked yet.");
+    });
+
+    it("Checking claim by time", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      await expect(stakeToken.connect(user1).claim()).to.be.revertedWith("It's been less than an hour.");
+    });
+
+    it("Repeated unstake", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      const aDay = 60 * 60 * 24;
+      await timeMovement(aDay);
+      await stakeToken.connect(user1).unstake();
+      await expect(stakeToken.connect(user1).unstake()).to.be.revertedWith("Your tokens is not staked yet.");
+    });
+
+    it("Checking unstake by time", async function () {
+      const { stakeToken, owner, user1, user2, user3, totalSupply, defaultUserBalance } = await loadFixture(deployStakeTokenAndPrepare);
+      await stakeToken.connect(user1).stake(defaultUserBalance);
+      const halfDay = 60 * 60 * 12;
+      await timeMovement(halfDay);
+      await expect(stakeToken.connect(user1).unstake()).to.be.revertedWith("The time for unstake has not yet come.");
+    });
+  });
+ 
+});
